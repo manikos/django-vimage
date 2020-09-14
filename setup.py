3f5aee7f1d9510c30e20997205d89f1f950189de
Nikos Mavrakis <mavrakis.n@gmail.com>
2020-09-14 12:29:11 +0300

    Bump version: 1.0.3 → 1.1.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     packages=find_packages(
         include=('vimage', 'vimage.*'),
     ),
-    version='1.0.3',
+    version='1.1.0',
     description="""
     Image validation (for the Django Admin) as a breeze. 
     Validations on: Size, Dimensions, Format and Aspect Ratio.
